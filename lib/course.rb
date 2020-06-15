d65f351575d723eba0508aa4288c4a7d84a608f1
Tanya Hodges <tanyac52003@gmail.com>
2020-06-15 15:51:29 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -3,8 +3,16 @@ class Course
   
   @@all = []
   
+  def initialize
+    @@all << self 
+  end 
   
+  def self.all 
+    @@all 
+  end 
   
-  
+  def self.reset_all
+    @@all.clear
+  end 
 end 
 
